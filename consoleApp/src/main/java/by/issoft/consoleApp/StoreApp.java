f10ec603722baf10da1430e245da456fe83a7bcb
Artem Mandrukevich <artemmandrukevich@coherentsolutions.com>
2022-05-03 15:14:54 +0300

    Interaction with Store

--- consoleApp/src/main/java/by/issoft/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/by/issoft/consoleApp/StoreApp.java
@@ -2,21 +2,22 @@ package by.issoft.consoleApp;
 
 import by.issoft.store.Helper.StoreHelper;
 import by.issoft.store.Store;
-import by.issoft.tools.sort.StoreComparator;
+import by.issoft.store.Helper.StoreInteraction;
+import java.io.IOException;
+
 
 public class StoreApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Store store = new Store();
         StoreHelper sh = new StoreHelper(store);
-        StoreComparator storeComparator = new StoreComparator();
+        StoreInteraction storeInteraction = new StoreInteraction(store);
 
         sh.fillOutProductList();
         store.printProductFromCategory();
 
-        store.printProduct(storeComparator.sortProduct(store.getAllProducts()));
-        store.printProduct(storeComparator.top5ProductPrice(store.getAllProducts()));
+        storeInteraction.SortProducts();
 
     }
 }
